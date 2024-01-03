73eebe825ad1fea3696b809685c95d73cb493a36
Moeinich <danielbang35@gmail.com>
2024-01-03 07:10:19 +0100

    OptionType PERCENTAGE

--- src/main/java/helpers/utils/OptionType.java
+++ src/main/java/helpers/utils/OptionType.java
@@ -3,6 +3,7 @@ package helpers.utils;
 /**
  * Defines the type of options available in script configuration.
  * This enum is used to specify the data type of a configuration parameter.
+ * They return their respective values as a String.
  */
 public enum OptionType {
 
@@ -20,6 +21,10 @@ public enum OptionType {
          * Represents a configuration option of type Boolean.
          */
         BOOLEAN,
+        /**
+         * Represents a configuration option of type Slider.
+         */
+        PERCENTAGE,
 
         /**
          * Represents a configuration option related to banking functionality.
