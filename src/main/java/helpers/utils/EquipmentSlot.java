f271e88711dab1aa1f8058cfe97859df3b480448
Moeinich <danielbang35@gmail.com>
2024-01-19 09:55:23 +0100

    Create EquipmentSlot.java

---
+++ src/main/java/helpers/utils/EquipmentSlot.java
@@ -0,0 +1,15 @@
+package helpers.utils;
+
+public enum EquipmentSlot {
+    HEAD,
+    CAPE,
+    NECK,
+    AMMUNITION,
+    WEAPON,
+    SHIELD,
+    BODY,
+    LEGS,
+    HANDS,
+    FEET,
+    RING
+}
