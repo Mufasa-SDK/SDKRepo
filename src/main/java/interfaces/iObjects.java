a312c492094aaf69b1c9b9e56fd00ae8d76d9a66
Moeinich <danielbang35@gmail.com>
2024-01-22 11:07:26 +0100

    Update iObjects.java

--- src/main/java/interfaces/iObjects.java
+++ src/main/java/interfaces/iObjects.java
@@ -11,30 +11,27 @@ public interface iObjects {
     /**
      * Finds the first game object within a specified tile radius from the center of the game view.
      *
-     * @param device     Identifier for the device or screen to search on.
      * @param tileRadius The radius, in terms of tiles, to search within from the center.
      * @param filePath   Path to the image file used for object recognition.
      * @return A Rectangle representing the found object's location and size; returns null if no object is found.
      */
-    Rectangle within(String device, int tileRadius, String filePath);
+    Rectangle within(int tileRadius, String filePath);
 
     /**
      * Finds game objects within a specified area, either returning the first found or all within the area.
      *
-     * @param device     Identifier for the device or screen to search on.
      * @param rect       The Rectangle area within which to search for objects.
      * @param returnAll  If true, returns all objects within the area; otherwise, returns only the first found object.
      * @param filePath   Path to the image file used for object recognition.
      * @return A list of Rectangles representing the locations and sizes of found objects; returns an empty list if no objects are found.
      */
-    java.util.List<Rectangle> within(String device, Rectangle rect, boolean returnAll, String filePath);
+    java.util.List<Rectangle> within(Rectangle rect, boolean returnAll, String filePath);
 
     /**
      * Finds the game object nearest to the center of the game view.
      *
-     * @param device   Identifier for the device or screen to search on.
      * @param filePath Path to the image file used for object recognition.
      * @return A Rectangle representing the nearest object's location and size; returns null if no object is found.
      */
-    Rectangle getNearest(String device, String filePath);
+    Rectangle getNearest(String filePath);
 }
