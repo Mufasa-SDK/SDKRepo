f18dd822a60eac67737851c35505a239d0ca04ac
Moeinich <danielbang35@gmail.com>
2024-01-23 13:41:01 +0100

    Update Area.java

--- src/main/java/helpers/utils/Area.java
+++ src/main/java/helpers/utils/Area.java
@@ -22,9 +22,18 @@ public class Area {
 
     // Method to check if a point is within this area
     public boolean contains(Point point) {
-        // Check if the point is within the horizontal and vertical bounds of the area
-        return point.x >= bottomLeftTile.getTopLeft().x && point.x <= topRightTile.getBottomRight().x &&
-                point.y >= bottomLeftTile.getTopLeft().y && point.y <= topRightTile.getBottomRight().y;
+        // Top-left corner of the top-right tile
+        Point topLeftOfTopRightTile = topRightTile.getTopLeft();
+
+        // Bottom-right corner of the bottom-left tile
+        Point bottomRightOfBottomLeftTile = new Point(
+                bottomLeftTile.getTopLeft().x + bottomLeftTile.getWidth() - 1,
+                bottomLeftTile.getTopLeft().y + bottomLeftTile.getHeight() - 1
+        );
+
+        // Check if the point is within the bounds
+        return point.x >= topLeftOfTopRightTile.x && point.x <= bottomRightOfBottomLeftTile.x &&
+                point.y >= topLeftOfTopRightTile.y && point.y <= bottomRightOfBottomLeftTile.y;
     }
 
     // Method to get the width of the area
