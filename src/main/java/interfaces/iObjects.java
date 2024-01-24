8afbe1299cc03f6d65aa635e8f7119e541f467e5
Moeinich <danielbang35@gmail.com>
2024-01-24 12:44:59 +0100

    remove Tile & Area
    This will literally be a complete overhaul. We need to think of some other way

--- src/main/java/interfaces/iObjects.java
+++ src/main/java/interfaces/iObjects.java
@@ -14,6 +14,7 @@ public interface iObjects {
      *
      * @param tileRadius The radius, in terms of tiles, to search within from the center.
      * @param filePath   Path to the image file used for object recognition.
+     * @param returnAll  If true, returns all objects within the area; otherwise, returns only the first found object.
      * @return A list of Rectangles representing the locations and sizes of found objects; returns an empty list if no objects are found.
      */
     List<Rectangle> within(int tileRadius, boolean returnAll, String filePath);
