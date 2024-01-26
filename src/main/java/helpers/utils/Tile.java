86b6860c16f4d230a5742f27397565f86f0371de
Moeinich <danielbang35@gmail.com>
2024-01-26 11:24:52 +0100

    Create Tile.java

---
+++ src/main/java/helpers/utils/Tile.java
@@ -0,0 +1,18 @@
+package helpers.utils;
+
+public class Tile {
+    private int x;
+    private int y;
+
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x() {
+        return x;
+    }
+    public int y() {
+        return y;
+    }
+}
