5341f0b99e2425e436b436c187a8542817fedb1f
Moeinich <danielbang35@gmail.com>
2024-01-26 11:26:20 +0100

    Tile/Tile JDocs

--- src/main/java/helpers/utils/Tile.java
+++ src/main/java/helpers/utils/Tile.java
@@ -1,17 +1,37 @@
 package helpers.utils;
 
+/**
+ * Represents a tile with x and y coordinates.
+ */
 public class Tile {
     private int x;
     private int y;
 
+    /**
+     * Constructs a new Tile with the specified x and y coordinates.
+     *
+     * @param x The x coordinate of the tile.
+     * @param y The y coordinate of the tile.
+     */
     public Tile(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Gets the x coordinate of the tile.
+     *
+     * @return The x coordinate of the tile.
+     */
     public int x() {
         return x;
     }
+
+    /**
+     * Gets the y coordinate of the tile.
+     *
+     * @return The y coordinate of the tile.
+     */
     public int y() {
         return y;
     }
