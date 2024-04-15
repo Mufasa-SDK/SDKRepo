7eaecd938fbde5d1402849a20d0cd1455f541080
Moeinich <danielbang35@gmail.com>
2024-04-15 12:38:24 +0200

    Skills enum

---
+++ src/main/java/helpers/utils/Skills.java
@@ -0,0 +1,29 @@
+package helpers.utils;
+
+/**
+ * The {@code Skills} enum represents all the skills available in OSRS
+ */
+public enum Skills {
+    AGILITY,
+    ATTACK,
+    STRENGHT,
+    DEFENCE,
+    RANGED,
+    COOKING,
+    CRAFTING,
+    CONSTRUCTION,
+    FARMING,
+    FIREMAKING,
+    FISHING,
+    FLETCHING,
+    HERBLORE,
+    HUNTER,
+    MAGIC,
+    MINING,
+    PRAYER,
+    RUNECRAFTING,
+    SLAYER,
+    SMITHING,
+    THIEVING,
+    WOODCUTTING,
+}
