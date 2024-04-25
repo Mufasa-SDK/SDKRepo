ee789a2e4ad81325fe089c5254c9792f0b798b8c
Moeinich <danielbang35@gmail.com>
2024-04-25 17:26:06 +0200

    extended the color finding methods with exclusions

--- src/main/java/interfaces/iClient.java
+++ src/main/java/interfaces/iClient.java
@@ -146,40 +146,112 @@ public interface iClient {
     boolean isColorAtPoint(Color targetColor, Point pointToSearch, int tolerance);
 
     /**
-     * Checks if a specified color is found on the game screen
+     * Retrieves a list of rectangles containing objects of specified colors with the given tolerance level.
      *
-     * @param colors a list of colors you want to search for
-     * @param tolerance the color tolerance for matching the target color
-     * @return a list of Rectangles that contain the found color clusters
+     * @param colors        The list of colors to search for.
+     * @param tolerance     The tolerance level for color matching.
+     * @return              A list of rectangles containing objects of the specified colors.
      */
-    List<Rectangle> getObjectsFromColor(List<Color> colors, int tolerance);
+    public List<Rectangle> getObjectsFromColor(List<Color> colors, int tolerance);
 
     /**
-     * Checks if a specified color is found on the game screen within the given rectangle
+     * Retrieves a list of rectangles containing objects of specified colors with the given tolerance level,
+     * excluding objects of specified exclusion colors with the given exclusion tolerance.
      *
-     * @param colors a list of colors you want to search for
-     * @param searchRect the rectangle to search for color clusters in
-     * @param tolerance the color tolerance for matching the target color
-     * @return a list of Rectangles that contain the found color clusters
+     * @param colors            The list of colors to search for.
+     * @param tolerance         The tolerance level for color matching.
+     * @param exclusionColors   The list of colors to exclude from the search.
+     * @param exclusionTolerance    The tolerance level for excluding colors.
+     * @return                  A list of rectangles containing objects of the specified colors, excluding those
+     *                          of the specified exclusion colors.
      */
-    List<Rectangle> getObjectsFromColorsInRect(List<Color> colors, Rectangle searchRect, int tolerance);
+    public List<Rectangle> getObjectsFromColor(List<Color> colors, int tolerance, List<Color> exclusionColors, int exclusionTolerance);
 
     /**
-     * Checks if a specified color is found on the game screen within the given rectangle
+     * Retrieves a list of rectangles containing objects of specified colors within the given search rectangle.
      *
-     * @param colors a list of colors you want to search for
-     * @param tolerance the color tolerance for matching the target color
-     * @return a list of Point that has the found color
+     * @param colors        The list of colors to search for.
+     * @param searchRect    The rectangle within which to search for the colors.
+     * @param tolerance     The tolerance level for color matching.
+     * @return              A list of rectangles containing objects of the specified colors.
+     */
+    public List<Rectangle> getObjectsFromColorsInRect(List<Color> colors, Rectangle searchRect, int tolerance);
+
+    /**
+     * Retrieves a list of rectangles containing objects of specified colors within the given search rectangle,
+     * excluding objects of specified exclusion colors.
+     *
+     * @param colors            The list of colors to search for.
+     * @param searchRect        The rectangle within which to search for the colors.
+     * @param tolerance         The tolerance level for color matching.
+     * @param exclusionColors   The list of colors to exclude from the search.
+     * @param exclusionTolerance    The tolerance level for excluding colors.
+     * @return                  A list of rectangles containing objects of the specified colors, excluding those
+     *                          of the specified exclusion colors.
+     */
+    public List<Rectangle> getObjectsFromColorsInRect(List<Color> colors, Rectangle searchRect, int tolerance, List<Color> exclusionColors, int exclusionTolerance);
+
+    /**
+     * Retrieves points on the current emulator screen where specified colors are found,
+     * considering a specified tolerance for color matching. This method acts as a convenience
+     * wrapper by fetching the current emulator ID and delegating the color search.
+     *
+     * @param colors A list of {@link Color} objects representing the colors to search for.
+     * @param tolerance An int value representing the tolerance used for matching the colors.
+     *                  The higher the tolerance, the more variations of the color will be accepted.
+     * @return A List of {@link Point} objects, each point representing the location on the screen
+     *         where one of the specified colors was detected.
      */
     public List<Point> getPointsFromColors(List<Color> colors, int tolerance);
 
     /**
-     * Checks if a specified color is found on the game screen within the given rectangle
+     * Retrieves points on the current emulator screen where specified colors are found,
+     * excluding specified colors within a certain tolerance, and considering a specified
+     * tolerance for the target colors. This method provides an enhanced search by excluding
+     * unwanted colors and is a convenience wrapper that fetches the current emulator ID
+     * and delegates the color search.
      *
-     * @param colors a list of colors you want to search for
-     * @param searchRect the rectangle to search for color clusters in
-     * @param tolerance the color tolerance for matching the target color
-     * @return a list of Point that has the found color
+     * @param colors A list of {@link Color} objects representing the colors to search for.
+     * @param tolerance An int value representing the tolerance used for matching the target colors.
+     *                  The higher the tolerance, the more variations of the color will be accepted.
+     * @param exclusionColors A list of {@link Color} objects representing the colors to be excluded from the search.
+     * @param exclusionTolerance An int value representing the tolerance used to exclude the unwanted colors.
+     *                           Similar to 'tolerance', the higher this value, the more variations of the exclusion colors will be ignored.
+     * @return A List of {@link Point} objects, each point representing the location on the screen
+     *         where one of the specified target colors was detected, excluding any matches found
+     *         within the exclusion tolerance for the colors specified in 'exclusionColors'.
+     */
+    public List<Point> getPointsFromColors(List<Color> colors, int tolerance, List<Color> exclusionColors, int exclusionTolerance);
+
+
+    /**
+     * Searches for specified colors within a defined rectangular area of the game screen,
+     * taking into account a specified color tolerance for matching. This method will return
+     * a list of points where the colors were found.
+     *
+     * @param colors A list of {@link Color} objects representing the colors to search for.
+     * @param searchRect A {@link Rectangle} defining the area within which to search for the colors.
+     * @param tolerance An int value representing the tolerance used to match the colors.
+     *               The higher the tolerance, the more variations of the color will be accepted.
+     * @return A List of {@link Point} objects, each representing a pixel location where one of the specified colors was detected.
      */
     public List<Point> getPointsFromColorsInRect(List<Color> colors, Rectangle searchRect, int tolerance);
+
+    /**
+     * Searches for specified colors within a defined rectangular area of the game screen,
+     * excluding specified colors within a certain tolerance, and taking into account a specified
+     * color tolerance for the target colors. This method returns a list of points where the target
+     * colors were found, excluding the specified unwanted colors.
+     *
+     * @param colors A list of {@link Color} objects representing the colors to search for.
+     * @param searchRect A {@link Rectangle} defining the area within which to search for the colors.
+     * @param tolerance An int value representing the tolerance used to match the target colors.
+     *               The higher the tolerance, the more variations of the color will be accepted.
+     * @param exclusionColors A list of {@link Color} objects representing the colors to be excluded from the search.
+     * @param exclusionTolerance An int value representing the tolerance used to exclude the unwanted colors.
+     *               Similar to 'tolerance', the higher this value, the more variations of the exclusion colors will be ignored.
+     * @return A List of {@link Point} objects, each representing a pixel location where one of the specified target colors was detected,
+     *         excluding any matches found within the exclusion tolerance for the colors specified in 'exclusionColors'.
+     */
+    public List<Point> getPointsFromColorsInRect(List<Color> colors, Rectangle searchRect, int tolerance, List<Color> exclusionColors, int exclusionTolerance);
 }
