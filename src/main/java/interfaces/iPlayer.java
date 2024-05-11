709bca1765f86e56e7a17acebf11432106957981
Davy <davykanningstudent@gmail.com>
2024-05-11 19:29:16 +0200

    antiAFK and items added

--- src/main/java/interfaces/iPlayer.java
+++ src/main/java/interfaces/iPlayer.java
@@ -151,4 +151,10 @@ public interface iPlayer {
      */
     void disableAutoRetaliate();
 
+    /**
+     * Performs an Anti-AFK action to prevent logging off because of being idle.
+     *
+     */
+    void performAntiAFK();
+
 }
