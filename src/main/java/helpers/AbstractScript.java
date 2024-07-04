32617ecec605a6ad178f0159069ca7b8673725f6
Moeinich <danielbang35@gmail.com>
2024-07-04 13:03:50 +0200

    revert revert revert

--- src/main/java/helpers/AbstractScript.java
+++ src/main/java/helpers/AbstractScript.java
@@ -1,14 +1,11 @@
 package helpers;
 
 import interfaces.*;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-
 import java.util.Map;
 
 public abstract class AbstractScript {
     private final Object pauseLock = new Object();
-    private final BooleanProperty paused = new SimpleBooleanProperty(false);
+    private boolean paused = false;
     private boolean isInitialized = false;
 
     public void initialize(iLogger logger, iBank bank, iClient client, iCondition condition, iDepositBox depositBox, iEquipment equipment, iGame game, iGameTabs gameTabs, iInventory inventory, iLogin login, iLogout logout, iMagic magic, iOverlay overlay, iPlayer player, iPrayer prayer, iStats stats, iWalker walker, iXPBar xpBar, iChatbox chatbox, iObjects objects, iScript script) {
@@ -26,23 +23,18 @@ public abstract class AbstractScript {
     }
 
     public boolean isPaused() {
-        return paused.get();
+        return paused;
     }
 
     // Method to set the paused state
-    public void setPaused(boolean paused) {
-        synchronized (pauseLock) {
-            this.paused.set(paused);
-            if (!paused) {
-                pauseLock.notifyAll(); // Wake up the thread if it's waiting
-            }
-        }
+    public void setPaused(boolean state) {
+        paused = state;
     }
 
     // Method to check and wait if paused
     protected void checkAndPause() {
         synchronized (pauseLock) {
-            while (paused.get()) {
+            while (paused) {
                 try {
                     pauseLock.wait(); // Wait until notified
                 } catch (InterruptedException e) {
@@ -54,7 +46,7 @@ public abstract class AbstractScript {
         }
     }
 
-    public void running() {
+    public void run() {
         if (!Thread.interrupted()) {
             checkAndPause();
             poll();
