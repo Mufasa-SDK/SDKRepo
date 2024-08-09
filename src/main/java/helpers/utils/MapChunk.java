44b0d1a78c4f78c13bcd3e823b93cc5932b20f18
Moeinich <danielbang35@gmail.com>
2024-08-09 07:59:55 +0200

    MapChunk

--- src/main/java/helpers/utils/MapChunk.java
+++ src/main/java/helpers/utils/MapChunk.java
@@ -5,15 +5,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MapChunk {
-    private final List<String> coordinates;
+    private final List<String> chunks;
+    private final List<String> planes;
 
     // Constructor to initialize MapChunk with one or multiple coordinates
-    public MapChunk(String... chunks) {
-        this.coordinates = new ArrayList<>(Arrays.asList(chunks));
+    public MapChunk(String[] chunks, String... planes) {
+        this.chunks = new ArrayList<>(Arrays.asList(chunks));
+        this.planes = new ArrayList<>();
+        for (String plane : planes) {
+            if (isValidPlane(plane)) {
+                this.planes.add(plane);
+            } else {
+                throw new IllegalArgumentException("Plane must be between 0 and 3: " + plane);
+            }
+        }
     }
 
-    // Method to get the list of coordinates
+    // Method to check if a plane is valid (0-3)
+    private boolean isValidPlane(String plane) {
+        return plane.matches("[0-3]");
+    }
+
+    // Method to get the list of chunks
     public List<String> getChunks() {
-        return coordinates;
+        return chunks;
+    }
+
+    // Method to get the list of planes
+    public List<String> getPlanes() {
+        return planes;
     }
 }
