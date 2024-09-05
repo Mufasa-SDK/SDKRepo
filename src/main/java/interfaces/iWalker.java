88516995193da33752c3a2fbd97d1ded6a5c48ae
Moeinich <danielbang35@gmail.com>
2024-09-05 10:31:45 +0200

    walker cleanup

--- src/main/java/interfaces/iWalker.java
+++ src/main/java/interfaces/iWalker.java
@@ -1,5 +1,6 @@
 package interfaces;
 
+import helpers.utils.MapChunk;
 import helpers.utils.RegionBox;
 import helpers.utils.Tile;
 
@@ -11,44 +12,13 @@ import java.awt.*;
  * and walking along predefined paths within the game.
  */
 public interface iWalker {
-
     /**
-     * Configures the system to use a custom map file instead of the default worldmap.
      * This method must be called before any operations that require map data are performed
-     * to ensure the custom map is loaded and ready for use. If a custom map is not set up
-     * using this method, the system will revert to using the default map.
+     * to ensure the map is loaded and ready for use.
      *
-     * @param customMapfilePath The relative file path to the custom map file. Path must be relative to your script directory.
+     * @param mapChunk The mapchunk object to generate the map from.
      */
-    void setup(String customMapfilePath);
-
-    /**
-     * Configures the system to use a regionbox with the full worldmap.
-     * This method must be called before any operations that require map data are performed
-     * to ensure the custom map is loaded and ready for use. If a custom map is not set up
-     * using this method, the system will revert to using the default map.
-     *
-     * @param regionBox The regionbox you would like to use on the FULL worldmap
-     */
-    void setup(RegionBox regionBox);
-
-    /**
-     * Configures the system to use a custom map file instead of the default worldmap with a regionbox defined.
-     * This method must be called before any operations that require map data are performed
-     * to ensure the custom map is loaded and ready for use. If a custom map is not set up
-     * using this method, the system will revert to using the default map.
-     *
-     * @param customMapfilePath The relative file path to the custom map file. Path must be relative to your script directory.
-     * @param regionBox the regionBox you would like the walker to use by default.
-     */
-    void setup(String customMapfilePath, RegionBox regionBox);
-
-    /**
-     * Updates the region box for the walker. In case you need to swap it with a teleport, for example.
-     *
-     * @param regionBox The new region box to associate with the device.
-     */
-    void changeRegion(RegionBox regionBox);
+    void setup(MapChunk mapChunk);
 
     /**
      * Gets the player's current position.
@@ -58,36 +28,11 @@ public interface iWalker {
     Tile getPlayerPosition();
 
     /**
-     * Gets the player's current position within a specified region.
-     *
-     * @param region The region for which to determine the player's position.
-     * @return Tile representing the player's current position within the region.
-     */
-    Tile getPlayerPosition(RegionBox region);
-
-    /**
-     * Takes a step to a specified Tile. Needs to be visible on the minimap.
-     *
-     * @param worldmapPoint The tile to step to on the world map.
-     * @param region the RegionBox region you are in on the worldmap.
-     */
-    void step(Tile worldmapPoint, RegionBox region);
-
-    /**
      * Takes a step to a specified Tile without providing a search region. Needs to be visible on the minimap.
      *
      * @param worldmapPoint The tile to step to on the world map.
      */
     void step(Tile worldmapPoint);
-
-    /**
-     * Takes a step to a specified Tile. Needs to be visible on the minimap.
-     *
-     * @param worldmapPoint The tile to step to on the world map.
-     * @param region the RegionBox region you are in on the worldmap.
-     * @param actionWhileStepping a runnable to perform while waiting till destination is reached
-     */
-    void step(Tile worldmapPoint, RegionBox region, Runnable actionWhileStepping);
 
     /**
      * Takes a step to a specified Tile without providing a search region. Needs to be visible on the minimap.
@@ -114,41 +59,6 @@ public interface iWalker {
     boolean walkTo(Tile worldmapTile);
 
     /**
-     * Attempts to navigate to a specified world map tile within a given region. This
-     * method is similar to {@link #walkTo(Tile)} but takes an additional {@link RegionBox}
-     * parameter that specifies the region within which the destination tile is located.
-     *
-     * @param worldmapTile The {@link Tile} object representing the destination tile
-     * on the world map within the specified region.
-     * @param region The {@link RegionBox} defining the area within which the destination
-     * tile is located. This parameter helps refine the navigation process by providing
-     * additional context about the destination's location.
-     * @return {@code true} if the method successfully initiates movement towards the
-     * specified location within the given region, {@code false} otherwise. As with
-     * {@link #walkTo(Tile)}, success here indicates that the navigation action was
-     * initiated, not necessarily that the destination will be reached.
-     */
-    boolean walkTo(Tile worldmapTile, RegionBox region);
-
-    /**
-     * Attempts to navigate to a specified world map tile within a given region. This
-     * method is similar to {@link #walkTo(Tile)} but takes an additional {@link RegionBox}
-     * parameter that specifies the region within which the destination tile is located.
-     *
-     * @param worldmapTile The {@link Tile} object representing the destination tile
-     * on the world map within the specified region.
-     * @param region The {@link RegionBox} defining the area within which the destination
-     * tile is located. This parameter helps refine the navigation process by providing
-     * additional context about the destination's location.
-     * @param actionWhileWalking an action to be performed while walking
-     * @return {@code true} if the method successfully initiates movement towards the
-     * specified location within the given region, {@code false} otherwise. As with
-     * {@link #walkTo(Tile)}, success here indicates that the navigation action was
-     * initiated, not necessarily that the destination will be reached.
-     */
-    boolean walkTo(Tile worldmapTile, RegionBox region, Runnable actionWhileWalking);
-
-    /**
      * Attempts to navigate to a specified world map tile. This method calculates
      * the minimap point corresponding to the provided world map tile and performs
      * a tap action to initiate movement towards that location. The method includes
@@ -166,31 +76,12 @@ public interface iWalker {
     boolean walkTo(Tile worldmapTile, Runnable actionWhileWalking);
 
     /**
-     * Walks along a specified path within a given region.
-     *
-     * @param region The region within which to walk the path.
-     * @param path An array of Tiles representing the path to walk.
-     * @return Boolean indicating whether the path was successfully walked.
-     */
-    Boolean walkPath(RegionBox region, Tile[] path);
-
-    /**
      * Walks along a specified path without providing a region. This should primarily be used if using custom maps
      *
      * @param path An array of Tiles representing the path to walk.
      * @return Boolean indicating whether the path was successfully walked.
      */
     Boolean walkPath(Tile[] path);
-
-    /**
-     * Walks along a specified path within a given region.
-     *
-     * @param region The region within which to walk the path.
-     * @param path An array of Tiles representing the path to walk.
-     * @param whileRunning an action to be performed while waiting between points
-     * @return Boolean indicating whether the path was successfully walked.
-     */
-    Boolean walkPath(RegionBox region, Tile[] path, Runnable whileRunning);
 
     /**
      * Walks along a specified path without providing a region. This should primarily be used if using custom maps
@@ -209,30 +100,6 @@ public interface iWalker {
      * @return Boolean indicating whether the destination was successfully reached.
      */
     boolean webWalk(Tile destinationTile);
-
-    /**
-     * Walks to a specified destination tile within a given region box using a dynamic path calculation. This method
-     * calculates the best possible path to the end destination tile, optionally including a RegionBox for the region
-     * of interest (ROI) on the map.
-     *
-     * @param destinationTile The Tile representing the end destination.
-     * @param region The RegionBox representing the region of interest (ROI) on the map.
-     * @return Boolean indicating whether the destination was successfully reached.
-     */
-    boolean webWalk(Tile destinationTile, RegionBox region);
-
-
-    /**
-     * Determines if the specified Tile is visible on the minimap.
-     *
-     * This method checks whether a given tile can be reached based on its visibility on the minimap,
-     * implying that it is within a certain range or not obstructed.
-     *
-     * @param tile The Tile to check for reachability.
-     * @param mapRegion The mapregion to get player position from.
-     * @return {@code true} if the tile is visible and potentially reachable on the minimap; {@code false} otherwise.
-     */
-    boolean isReachable(Tile tile, RegionBox mapRegion);
 
     /**
      * Determines if the specified Tile is visible on the minimap, using the already set-up Region from Walker.setup.
