742292a3547617e3b0d7a2990f1f109e9641ded1
Davy <davykanningstudent@gmail.com>
2024-11-01 16:30:53 +0100

    GameTabs enum

---
+++ src/main/java/helpers/utils/GameTabs.java
@@ -0,0 +1,21 @@
+package helpers.utils;
+
+/**
+ * Defines the type of options available when querying for a tab of the game UI.
+ */
+public enum GameTabs {
+    ACCOUNT,
+    CLAN,
+    COMBAT,
+    EMOTES,
+    EQUIP,
+    FRIENDS,
+    INVENTORY,
+    LOGOUT,
+    MAGIC,
+    MUSIC,
+    PRAYER,
+    QUESTS,
+    SETTINGS,
+    STATS
+}
