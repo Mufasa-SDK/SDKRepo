c637408ff5f82484db8078ff6b2b265e08b5d4e7
Davy <davykanningstudent@gmail.com>
2025-01-08 11:09:27 +0100

    Condition.sleep allow min/max

--- src/main/java/interfaces/iCondition.java
+++ src/main/java/interfaces/iCondition.java
@@ -17,6 +17,15 @@ public interface iCondition {
     void sleep(int intervalMillis);
 
     /**
+     * Causes the current thread to sleep for a random amount of time.
+     * This is a blocking operation that pauses the thread for the given duration.
+     *
+     * @param minMillis the minimum duration to sleep in milliseconds.
+     * @param maxMillis the maximum duration to sleep in milliseconds.
+     */
+    void sleep(int minMillis, int maxMillis);
+
+    /**
      * Waits for a specified condition to be met, checking at regular intervals, up to a maximum number of attempts.
      * The method repeatedly checks the given condition until it returns true or the maximum number of attempts is reached.
      *
