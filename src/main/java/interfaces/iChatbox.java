fae6f7754abbcef622aa0506d899154cc611068c
Davy <davykanningstudent@gmail.com>
2025-02-16 12:35:30 +0100

    per font methods

--- src/main/java/interfaces/iChatbox.java
+++ src/main/java/interfaces/iChatbox.java
@@ -1,7 +1,5 @@
 package interfaces;
 
-import helpers.utils.FontName;
-
 import java.awt.*;
 import java.util.Map;
 
@@ -130,10 +128,54 @@ public interface iChatbox {
      * @return A string with the OCR results.
      *
      * @param areaToOCR A java.awt.Rectangle with the area you want to perform OCR on.
-     * @param font A font from the FontName enum to read the area with
      * @param colors A list of java awt colors used to find text with
      */
-    String readText(Rectangle areaToOCR, FontName font, java.util.List<java.awt.Color> colors);
+    String readAnyText(Rectangle areaToOCR, java.util.List<java.awt.Color> colors);
+
+    /**
+     * Reads the specific area using the Bold 12 font, and returns what it was able to read.
+     * @return A string with the OCR results.
+     *
+     * @param areaToOCR A java.awt.Rectangle with the area you want to perform OCR on.
+     * @param colors A list of java awt colors used to find text with
+     */
+    String readBold12Text(Rectangle areaToOCR, java.util.List<java.awt.Color> colors);
+
+    /**
+     * Reads the specific area using the Plain 11 font, and returns what it was able to read.
+     * @return A string with the OCR results.
+     *
+     * @param areaToOCR A java.awt.Rectangle with the area you want to perform OCR on.
+     * @param colors A list of java awt colors used to find text with
+     */
+    String readPlain11Text(Rectangle areaToOCR, java.util.List<java.awt.Color> colors);
+
+    /**
+     * Reads the specific area using the Plain 12 font, and returns what it was able to read.
+     * @return A string with the OCR results.
+     *
+     * @param areaToOCR A java.awt.Rectangle with the area you want to perform OCR on.
+     * @param colors A list of java awt colors used to find text with
+     */
+    String readPlain12Text(Rectangle areaToOCR, java.util.List<java.awt.Color> colors);
+
+    /**
+     * Reads the specific area using the Quill font, and returns what it was able to read.
+     * @return A string with the OCR results.
+     *
+     * @param areaToOCR A java.awt.Rectangle with the area you want to perform OCR on.
+     * @param colors A list of java awt colors used to find text with
+     */
+    String readQuillText(Rectangle areaToOCR, java.util.List<java.awt.Color> colors);
+
+    /**
+     * Reads the specific area using the Quill 8 font, and returns what it was able to read.
+     * @return A string with the OCR results.
+     *
+     * @param areaToOCR A java.awt.Rectangle with the area you want to perform OCR on.
+     * @param colors A list of java awt colors used to find text with
+     */
+    String readQuill8Text(Rectangle areaToOCR, java.util.List<java.awt.Color> colors);
 
     /**
      * Reads the specific area, and returns the digits (0-9) it was able to read.
