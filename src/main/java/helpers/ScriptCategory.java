4d9c3669db220e8af41e8a59855f0b51c21a58a8
Moeinich <danielbang35@gmail.com>
2025-05-23 07:11:24 +0200

    Revert "Only local category remains."
    This reverts commit c65710b883ba21485ac483353eaf5208c5fd3b0c.

--- src/main/java/helpers/ScriptCategory.java
+++ src/main/java/helpers/ScriptCategory.java
@@ -5,5 +5,39 @@ package helpers;
  * Each category corresponds to a specific skill or type of activity.
  */
 public enum ScriptCategory {
+    Agility,
+    Combat,
+    Cooking,
+    Crafting,
+    Construction,
+    Farming,
+    Firemaking,
+    Fishing,
+    Fletching,
+    Herblore,
+    Hunter,
+    Magic,
+    Mining,
+    Prayer,
+    Runecrafting,
+    Slayer,
+    Smithing,
+    Thieving,
+    Woodcutting,
+    Moneymaking,
+    Minigames,
+    Misc,
+    Ironman,
+    /**
+     * Represents scripts that are made custom for users and only accessible by those users
+     */
+    Custom,
+    /**
+     * Represents scripts that are currently still in pre-release/beta.
+     */
+    Beta,
+    /**
+     * Represents scripts that are executed locally.
+     */
     Local;
 }
